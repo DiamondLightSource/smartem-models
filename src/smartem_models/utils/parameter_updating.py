b3f476180f2367b224db578de78b6d6302a3ac32
Daniel Hatton <daniel.hatton@diamond.ac.uk>
2025-05-27 22:11:14 +0100

    Utility functions for Bayesian updates of probability distributions across data parameterisations

--- src/smartem_models/utils/parameter_updating.py
+++ src/smartem_models/utils/parameter_updating.py
@@ -1,3 +1,5 @@
+from functools import lru_cache
+
 import numpy as np
 import scipy.stats
 
@@ -24,9 +26,9 @@ def grid_hist(grid: tuple[tuple[float, float, float]], coords: list[tuple], valu
     return flattened_hist.reshape((*grid_shape, most_hits))
 
 
-def init_distributions(grid: np.array, num_steps: int = 10) -> np.array:
-    means = np.nanmean(grid, axis=-1)
-    sdevs = np.nanstd(grid, axis=-1)
+def init_distributions(hist: np.array, num_steps: int = 10) -> np.array:
+    means = np.nanmean(hist, axis=-1)
+    sdevs = np.nanstd(hist, axis=-1)
     # if there was only one (non-nan value in the array take the standard deviation to be 0.5
     sdevs = np.where(sdevs == 0, 0.5, sdevs)
     dist_constructor = scipy.stats.truncnorm(loc=means, scale=sdevs, a=-means / sdevs, b=(1 - means) / sdevs)
@@ -35,3 +37,44 @@ def init_distributions(grid: np.array, num_steps: int = 10) -> np.array:
     for i in range(num_steps):
         res[i] = dist_constructor.pdf(dist_step * i + (dist_step / 2))
     return np.moveaxis(res, 0, -1)
+
+
+@lru_cache(maxsize=100)
+def _binary_bin_search(grid: tuple[tuple[float, float, float]], coord: tuple) -> tuple:
+    bin_index = [0 for _ in coord]
+    for i, (axis, x) in enumerate(zip(grid, coord, strict=False)):
+        edges = np.arange(axis[0], axis[1] + axis[2], axis[2])
+        indices = list(range(len(edges)))
+        while len(edges) > 1:
+            if x <= edges[len(edges) // 2]:
+                indices = indices[: len(edges) // 2]
+                edges = edges[: len(edges) // 2]
+            else:
+                indices = indices[len(edges) // 2 :]
+                edges = edges[len(edges) // 2 :]
+        bin_index[i] = indices[0]
+    return tuple(bin_index)
+
+
+def update_distributions(
+    dist: np.array, grid: tuple[tuple[float, float, float]], coord: tuple, quality: bool
+) -> np.array:
+    index = _binary_bin_search(grid, coord)
+    bin_dist = dist[index]
+    step = 1 / len(bin_dist)
+    midpoints = np.arange(step, 1 + step, step)
+    probs = np.array(
+        [step * p if (quality and p >= 0.5) or (not quality and p < 0.5) else (1 - step * p) for p in midpoints]
+    )
+    update_unnormalised = bin_dist * probs
+    update = update_unnormalised / np.sum(update_unnormalised)
+    dist[index] = update / step
+    return dist
+
+
+def score(dist: np.array, grid: tuple[tuple[float, float, float]], coord: tuple) -> float:
+    index = _binary_bin_search(grid, coord)
+    bin_dist = dist[index]
+    step = 1 / len(bin_dist)
+    midpoints = np.arange(step, 1 + step, step)
+    return np.sum(step * midpoints * bin_dist)
