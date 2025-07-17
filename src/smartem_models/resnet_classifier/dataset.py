c44a4006512a0d1990f4e7b3da111a8b7b4bbb3e
Daniel Hatton <daniel.hatton@diamond.ac.uk>
2025-07-17 16:02:13 +0100

    Add service code to evaluate grid squares from atlas tile images (#4)
    Uses pretrained ResNet with a binary classifier

---
+++ src/smartem_models/resnet_classifier/dataset.py
@@ -0,0 +1,79 @@
+from functools import lru_cache
+from pathlib import Path
+
+import mrcfile
+import numpy as np
+import tifffile
+from PIL import Image
+from pydantic import BaseModel, ConfigDict
+from smartem_decisions.model.database import Atlas, AtlasTile, AtlasTileGridSquarePosition, GridSquare
+from smartem_decisions.utils import setup_postgres_connection
+from sqlmodel import Session, select
+
+
+@lru_cache(maxsize=50)
+def _get_tile_image(base_filename: str, file_format: str, atlas_dir: str) -> Image.Image:
+    tile_file_name = f"{base_filename}.{file_format.lower()}"
+    tile_file = Path(atlas_dir) / tile_file_name
+    if tile_file.suffix == ".mrc":
+        data = mrcfile.read(tile_file)
+    else:
+        data = tifffile.imread(tile_file)
+    mean = np.mean(data)
+    sdev = np.std(data)
+    sigma_min = mean - 3 * sdev
+    sigma_max = mean + 3 * sdev
+    data = np.ndarray.copy(data)
+    data[data < sigma_min] = sigma_min
+    data[data > sigma_max] = sigma_max
+    data = data - data.min()
+    data = data / data.max()
+    data = data * 255
+    array = data.astype("uint8")
+    tile_img = Image.fromarray(array)
+    return tile_img
+
+
+class GridSquarePosition(BaseModel):
+    image: Image.Image
+    center_on_atlas: tuple[int, int]
+
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
+
+def grid_square_positions(grid_uuid: str, atlas_dir: str) -> dict[str, list[GridSquarePosition]]:
+    engine = setup_postgres_connection()
+    with Session(engine) as session:
+        positions = session.exec(
+            select(Atlas, AtlasTile, AtlasTileGridSquarePosition, GridSquare)
+            .where(Atlas.grid_uuid == grid_uuid)
+            .where(Atlas.uuid == AtlasTile.atlas_uuid)
+            .where(AtlasTile.uuid == AtlasTileGridSquarePosition.atlastile_uuid)
+            .where(AtlasTileGridSquarePosition.gridsquare_uuid == GridSquare.uuid)
+        ).all()
+
+    gs_imgs: dict[str, list[Image.Image]] = {}
+
+    for pos in positions:
+        d = (
+            pos[2].center_x - pos[2].size_width // 2,
+            pos[2].center_y - pos[2].size_height // 2,
+            pos[2].center_x + pos[2].size_width // 2,
+            pos[2].center_y + pos[2].size_height // 2,
+        )
+        if gs_imgs.get(pos[3].uuid) is None:
+            gs_imgs[pos[3].uuid] = [
+                GridSquarePosition(
+                    image=_get_tile_image(pos[1].base_filename, pos[1].file_format, atlas_dir).crop(d),
+                    center_on_atlas=(pos[3].center_x, pos[3].center_y),
+                )
+            ]
+        else:
+            gs_imgs[pos[3].uuid].append(
+                GridSquarePosition(
+                    image=_get_tile_image(pos[1].base_filename, pos[1].file_format, atlas_dir).crop(d),
+                    center_on_atlas=(pos[3].center_x, pos[3].center_y),
+                )
+            )
+
+    return gs_imgs
---
+++ src/smartem_models/resnet_classifier/service.py
@@ -0,0 +1,129 @@
+from pathlib import Path
+
+import numpy as np
+import torch
+from pydantic import BaseModel
+from smartem_decisions.model.database import Grid
+from smartem_decisions.utils import setup_postgres_connection
+from sqlmodel import Session, select
+from torchvision import models, transforms
+
+from smartem_models.resnet_classifier.dataset import GridSquarePosition, grid_square_positions
+from smartem_models.resnet_classifier.model import Net
+
+
+class InferenceParameters(BaseModel):
+    grid_uuid: str
+    model_path: Path
+    cpus: int = 4
+
+
+def infer(params: InferenceParameters) -> None:
+    torch.set_num_threads(params.cpus)
+    feature_extractor = models.resnet18(pretrained=True)
+    feature_extractor.conv1 = torch.nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+    feature_extractor.maxpool = torch.nn.Identity()
+    feature_extractor.fc = torch.nn.Identity()
+    model = Net(
+        feature_extractor,
+        num_classes=2,
+        centroid_size=512,
+        model_output_size=512,
+        length_scale=0.1,
+        gamma=0.999,
+    )
+    model.load_state_dict(torch.load(params.model_path, map_location=torch.device("cpu")))
+    model.eval()
+
+    engine = setup_postgres_connection()
+    with Session(engine) as session:
+        grid = session.exec(select(Grid).where(Grid.uuid == params.grid_uuid)).one()
+    gs_positions = grid_square_positions(params.grid_uuid, str(Path(grid.atlas_dir).parent))
+
+    boundaries = (
+        (
+            np.min([pos[0].center_on_atlas[0] for pos in gs_positions.values()]),
+            np.max([pos[0].center_on_atlas[0] for pos in gs_positions.values()]),
+        ),
+        (
+            np.min([pos[0].center_on_atlas[1] for pos in gs_positions.values()]),
+            np.max([pos[0].center_on_atlas[1] for pos in gs_positions.values()]),
+        ),
+    )
+
+    def _boundary_check(gs_name: str) -> tuple[bool, bool, bool, bool]:
+        loc = gs_positions[gs_name][0].center_on_atlas
+        brange = (
+            boundaries[0][1] - boundaries[0][0],
+            boundaries[1][1] - boundaries[1][0],
+        )
+        thresholds = (
+            (
+                boundaries[0][0] + 0.075 * brange[0],
+                boundaries[0][1] - 0.075 * brange[0],
+            ),
+            (
+                boundaries[1][0] + 0.075 * brange[1],
+                boundaries[1][1] - 0.075 * brange[1],
+            ),
+        )
+        return (
+            loc[0] > thresholds[0][0]
+            and loc[0] < thresholds[0][1]
+            and loc[1] > thresholds[1][0]
+            and loc[1] < thresholds[1][1]
+        )
+
+    def _area(positions: list[GridSquarePosition]) -> int:
+        return np.sum([(pos.image.size[0] * pos.image.size[1]) for pos in positions])
+
+    if np.std([_area(tmps) for tmps in gs_positions.values()]) > 50:
+        size_threshold = np.quantile(np.array([_area(tmps) for tmps in gs_positions.values()]), 0.5)
+    else:
+        size_threshold = 0
+
+    scores = {}
+    for s, pos in gs_positions.items():
+        img_transform = transforms.Compose(
+            [
+                transforms.ToPILImage(),
+                transforms.Resize((256, 256)),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            ]
+        )
+        score: float = 0
+        total_size_x = 0
+        total_size_y = 0
+        images = [p.image for p in pos]
+        if _area(pos) < size_threshold:
+            scores[s] = 0
+            continue
+        for template in images:
+            inputs = np.array(template)
+            inputs = inputs.astype("int16")
+            inputs = inputs - inputs.min()
+            inputs = (inputs * (255.0 / inputs.max())).astype(np.uint8)
+            tmp = torch.Tensor(inputs)
+            tmp = torch.reshape(tmp, (1, tmp.shape[0], tmp.shape[1]))
+            tmp = tmp.repeat(3, 1, 1)
+            tmp = img_transform(tmp)
+            tmp = (tmp - tmp.min()) / tmp.max()
+            img_variable = tmp.unsqueeze(0)
+
+            resnet_img, y_pred = model(img_variable)
+            confidence = y_pred.max(1)[0].detach().cpu().numpy()
+            _, predicted = torch.max(y_pred.data, 1)
+            predicted = predicted.detach().cpu().numpy()
+            predicted = 2.0 * predicted - 1
+            score += 0.5 * ((predicted * confidence)[0] + 1)
+            total_size_x += template.size[0]
+            total_size_y += template.size[1]
+
+        score /= len(pos)
+        scores[s] = score * np.sqrt(total_size_x * total_size_y) * (1 if _boundary_check(s) else 0)
+
+    max_score = np.max(list(scores.values()))
+    scores = {k: v / max_score for k, v in scores.items()}
+
+    return None
