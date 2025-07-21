615c592b34e4c99cbde94982f2570e9a4f59d0df
Daniel Hatton <daniel.hatton@diamond.ac.uk>
2025-07-21 12:01:19 +0100

    VAE clustering service for grid squares at atlas mag (#6)

--- src/smartem_models/clustering/calldata.py
+++ src/smartem_models/clustering/calldata.py
@@ -143,3 +143,80 @@ class SquareDataset(Dataset):
         sample = {"x1": sample, "x2": sample2, "lab": labs}
 
         return sample
+
+
+class SquareAtlasMagDataset(Dataset):
+    def __init__(
+        self, atlas_image_path: Path, grid_square_positions: dict[str, tuple[int, int]], size: int, transform=None
+    ):
+        self.atlas_img = read_img(atlas_image_path)
+        self.size = size
+        self.img_positions = []
+        self.labels = []
+        for i, (k, v) in enumerate(grid_square_positions.items()):
+            self.img_positions.append((k, v))
+            self.labels.append(i)
+
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.img_positions)
+
+    def __getitem__(self, idx: int) -> dict:
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        idx2 = random.randint(0, self.__len__() - 1)
+
+        x, y = self.img_positions[idx][1]
+        x2, y2 = self.img_positions[idx2][1]
+
+        yleft = y - self.size // 2
+        yright = y + self.size // 2
+        xleft = x - self.size // 2
+        xright = x + self.size // 2
+        xleft = xleft if xleft >= 0 else 0
+        xright = xright if xright >= 0 else 0
+        yleft = yleft if yleft >= 0 else 0
+        yright = yright if yright >= 0 else 0
+
+        yleft2 = y2 - self.size // 2
+        yright2 = y2 + self.size // 2
+        xleft2 = x2 - self.size // 2
+        xright2 = x2 + self.size // 2
+        xleft2 = xleft2 if xleft2 >= 0 else 0
+        xright2 = xright2 if xright2 >= 0 else 0
+        yleft2 = yleft2 if yleft2 >= 0 else 0
+        yright2 = yright2 if yright2 >= 0 else 0
+
+        sample = self.atlas_img[yleft:yright, xleft:xright].astype(np.float32)
+        sample2 = self.atlas_img[yleft2:yright2, xleft2:xright2].astype(np.float32)
+
+        if sample.shape[0] != sample.shape[1]:
+            if sample.shape[0] > sample.shape[1]:
+                sample = np.pad(sample, ((0, 0), (0, sample.shape[0] - sample.shape[1])))
+            else:
+                sample = np.pad(sample, ((0, sample.shape[1] - sample.shape[0]), (0, 0)))
+
+        if sample2.shape[0] != sample2.shape[1]:
+            if sample2.shape[0] > sample2.shape[1]:
+                sample2 = np.pad(sample2, ((0, 0), (0, sample2.shape[0] - sample2.shape[1])))
+            else:
+                sample2 = np.pad(sample2, ((0, sample2.shape[1] - sample2.shape[0]), (0, 0)))
+
+        sample = 2.0 * (sample / 255.0) - 1.0
+        sample2 = 2.0 * (sample2 / 255.0) - 1.0
+
+        sample = torch.from_numpy(np.expand_dims(sample, axis=0))
+        sample2 = torch.from_numpy(np.expand_dims(sample2, axis=0))
+        if self.transform:
+            sample = self.transform(sample)
+            sample2 = self.transform(sample2)
+
+        labs = self.labels[idx]
+
+        labs = torch.from_numpy(np.expand_dims(labs, axis=0))
+
+        sample = {"x1": sample, "x2": sample2, "lab": labs}
+
+        return sample
---
+++ src/smartem_models/clustering/service_atlas.py
@@ -0,0 +1,114 @@
+import pickle
+from pathlib import Path
+
+import numpy as np
+import torch
+from pydantic import BaseModel
+from sklearn.cluster import KMeans
+from smartem_decisions.model.database import Acquisition, Grid, GridSquare
+from smartem_decisions.utils import setup_postgres_connection
+from sqlmodel import Session, select
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+from torchvision import transforms
+
+from smartem_models.clustering.calldata import SquareAtlasMagDataset
+from smartem_models.clustering.grid_clustering import train
+from smartem_models.clustering.models import EIAE
+from smartem_models.clustering.service import _set_model_parameters
+from smartem_models.utils.parameter_updating_cluster import init_distributions
+
+model_name = "vae-atlas"
+
+
+class InitParameters(BaseModel):
+    grid_uuid: str
+    batch_size: int = 16
+    seed: int = 10
+    input_model: Path | None = None
+    input_dim: tuple[int, ...] = (1, 64, 64)
+    hidden_dims: tuple[int, ...] = (1, 16, 32, 64, 128)
+    latent_space_dim: int = 2
+    learning_rate: float = 0.0005
+    num_epochs: int = 500
+    model_output_path: str = ""
+    kmeans_output_path: str = ""
+
+
+def initialise(params: InitParameters) -> None:
+    engine = setup_postgres_connection()
+    with Session(engine) as session:
+        grid_squares = session.exec(select(GridSquare).where(GridSquare.grid_uuid == params.grid_uuid)).all()
+        atlas_path = (
+            session.exec(
+                select(Grid, Acquisition)
+                .where(Grid.uuid == params.grid_uuid)
+                .where(Grid.acquisition_uuid == Acquisition.uuid)
+            )
+            .all()[0][1]
+            .atlas_path
+        )
+        s = int(
+            1.1
+            * np.max([np.max([gs.size_width for gs in grid_squares]), np.max([gs.size_height for gs in grid_squares])])
+        )
+
+    square_positions = {gs.uuid: (gs.center_x, gs.center_y) for gs in grid_squares if gs.center_x is not None}
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    train_x = SquareAtlasMagDataset(
+        Path(atlas_path), square_positions, s, transform=transforms.Resize(params.input_dim[-1], antialias=True)
+    )
+    train_dataloader = DataLoader(train_x, batch_size=params.batch_size, shuffle=True, pin_memory=True)
+
+    np.random.seed(params.seed)
+    torch.manual_seed(params.seed)
+
+    model = EIAE(
+        alpha=torch.Tensor([[1.0, 1.0]]).to(device),
+        input_dims=params.input_dim,
+        hidden_dims=params.hidden_dims,
+        lat_dim=params.latent_space_dim,
+    )
+    if params.input_model:
+        model.load_state_dict(torch.load(params.input_model, map_location=device))
+    model.to(device)
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=params.learning_rate, betas=(0.9, 0.999))
+
+    losses = []
+    epoch_losses: list[float] = [10**15]
+
+    model.train()
+    for _epoch in range(params.num_epochs):
+        loss_record, epoch_loss = train(train_dataloader, model, optimizer, device)
+        losses += loss_record
+        epoch_losses.append(epoch_loss)
+
+    model.eval()
+
+    latent_coords = {}
+    for sample in train_dataloader:
+        coords = model.encode(Variable(sample["x1"]).to(device))[0].detach().cpu().numpy()
+        for i, label in enumerate(sample["lab"].detach().cpu().numpy().flatten()):
+            latent_coords[grid_squares[label].uuid] = coords[i]
+
+    num_clusters = len(latent_coords) // 5
+    labelled_coords = list(latent_coords.items())
+    kmeans = KMeans(n_clusters=num_clusters, random_state=0, n_init="auto").fit(
+        np.array([p[1] for p in labelled_coords])
+    )
+    labelled_coords = [(p[0], p[1], q) for p, q in zip(labelled_coords, kmeans.labels_, strict=False)]
+    hist = [[0.5 for p in labelled_coords if p[2] == label] for label in range(num_clusters)]
+    largest_cluster = np.max([len(p) for p in hist])
+    hist = [np.pad(p, (0, largest_cluster - len(p)), "constant", constant_values=(np.nan, np.nan)) for p in hist]
+    init_dists = init_distributions(hist)
+
+    if params.model_output_path:
+        torch.save(model.state_dict(), params.model_output_path)
+    if params.kmeans_output_path:
+        with open(params.kmeans_output_path, "wb") as pkl:
+            pickle.dump(kmeans, pkl)
+
+    _set_model_parameters(init_dists, labelled_coords, params.grid_uuid)
+
+    return None
