c2b8da792535dfd933648a2b0c90cee60da1b5bc
Daniel Hatton <daniel.hatton@diamond.ac.uk>
2025-08-19 11:55:31 +0100

    After hole inference resend registration messages for squares that may have been missed while training (#11)

--- src/smartem_models/clustering/service_holes.py
+++ src/smartem_models/clustering/service_holes.py
@@ -5,7 +5,9 @@ import numpy as np
 import torch
 from sklearn.cluster import KMeans
 from smartem_backend.model.database import FoilHole, GridSquare, QualityPredictionModelParameter
+from smartem_backend.mq_publisher import publish_gridsquare_registered
 from smartem_backend.utils import setup_postgres_connection
+from smartem_common.entity_status import GridSquareStatus
 from sqlmodel import Session, select
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
@@ -109,6 +111,18 @@ def initialise(params: InitParameters) -> None:
         with open(params.kmeans_output_path, "wb") as pkl:
             pickle.dump(kmeans, pkl)
 
+    # after writing files used in inference check for grid squares that need to have inference run
+    with Session(engine) as session:
+        registered_grid_squares = session.exec(
+            select(GridSquare)
+            .where(GridSquare.grid_uuid == params.grid_uuid)
+            .where(GridSquare.status == GridSquareStatus.REGISTERED)
+        ).all()
+    init_square_ids = [gs.uuid for gs in grid_squares]
+    for rs in registered_grid_squares:
+        if rs.uuid not in init_square_ids:
+            publish_gridsquare_registered(rs.uuid)
+
     _set_model_parameters(
         init_dists,
         labelled_coords,
@@ -119,6 +133,8 @@ def initialise(params: InitParameters) -> None:
 
 
 def infer(params: InferenceParameters):
+    if not params.model_path.is_file() or not params.kmeans_path.is_file():
+        return None
     torch.set_num_threads(params.num_threads)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = EIAE(
