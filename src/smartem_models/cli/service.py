eb7c2386b0d9471d9f3d78557ac54c2f3ff7c906
Daniel Hatton <daniel.hatton@diamond.ac.uk>
2026-09-16 16:34:03 +0100

    async compatibility for service starter

--- src/smartem_models/cli/service.py
+++ src/smartem_models/cli/service.py
@@ -1,15 +1,20 @@
+import asyncio
+
 import typer
 from backports.entry_points_selectable import entry_points
 
 from smartem_models.utils.consumer_wrapper import consume
 
 
-def start_service(level: str, service_name: str, queue_name: str):
+async def async_start_service(level: str, service_name: str, queue_name: str):
     func = entry_points().select(group=f"smartem_models.{level}", name=service_name)[0].load()
     validation_model = entry_points().select(group=f"smartem_models.{level}.signature", name=service_name)[0].load()
-    consume(func, queue_name, validation_model)
+    await consume(func, queue_name, validation_model)
 
 
 def run() -> None:
+    def start_service(level: str, service_name: str, queue_name: str):
+        asyncio.run(async_start_service(level, service_name, queue_name))
+
     typer.run(start_service)
     return None
