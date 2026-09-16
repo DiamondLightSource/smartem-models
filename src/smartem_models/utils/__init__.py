3b244576cede1829679fb21c2701acf1947709f3
Daniel Hatton <daniel.hatton@diamond.ac.uk>
2026-09-16 16:33:00 +0100

    catch file not found errors when reading images for more robustness

--- src/smartem_models/utils/__init__.py
+++ src/smartem_models/utils/__init__.py
@@ -21,12 +21,19 @@ def get_config():
 
 
 def read_img(img_path: Path, normalise: bool = True, crop: tuple[int] | None = None) -> np.array:
-    if img_path.suffix == ".mrc":
-        data = mrcfile.read(img_path)
-    elif img_path.suffix in (".tiff", ".tif"):
-        data = tifffile.imread(img_path)
-    else:
-        raise ValueError(f"Input images must be in MRC or TIFF format. Format {img_path.suffix} unrecognised")
+    try:
+        if img_path.suffix == ".mrc":
+            data = mrcfile.read(img_path)
+        elif img_path.suffix in (".tiff", ".tif"):
+            data = tifffile.imread(img_path)
+        else:
+            raise ValueError(f"Input images must be in MRC or TIFF format. Format {img_path.suffix} unrecognised")
+    except FileNotFoundError:
+        if img_path.suffix == ".mrc":
+            data = tifffile.imread(img_path.with_suffix(".tiff"))
+        elif img_path.suffix in (".tiff", ".tif"):
+            data = mrcfile.read(img_path.with_suffix(".mrc"))
+
     if not crop:
         crop = (np.min(data.shape), np.min(data.shape))
     left = (data.shape[0] - crop[0]) // 2
@@ -50,12 +57,18 @@ def read_img(img_path: Path, normalise: bool = True, crop: tuple[int] | None = N
 
 
 def read_square_img(img_path: Path) -> np.array:
-    if img_path.suffix == ".mrc":
-        data = mrcfile.read(img_path)
-    elif img_path.suffix in (".tiff", ".tif"):
-        data = tifffile.imread(img_path)
-    else:
-        raise ValueError(f"Input images must be in MRC or TIFF format. Format {img_path.suffix} unrecognised")
+    try:
+        if img_path.suffix == ".mrc":
+            data = mrcfile.read(img_path)
+        elif img_path.suffix in (".tiff", ".tif"):
+            data = tifffile.imread(img_path)
+        else:
+            raise ValueError(f"Input images must be in MRC or TIFF format. Format {img_path.suffix} unrecognised")
+    except FileNotFoundError:
+        if img_path.suffix == ".mrc":
+            data = tifffile.imread(img_path.with_suffix(".tiff"))
+        elif img_path.suffix in (".tiff", ".tif"):
+            data = mrcfile.read(img_path.with_suffix(".mrc"))
     return data
 
 
