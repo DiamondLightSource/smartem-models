f2697320df62398fa2a9864684bd5bea06c777a4
Daniel Hatton <daniel.hatton@diamond.ac.uk>
2026-09-16 16:35:06 +0100

    tweaks to default parameters (dae atlas)

--- src/smartem_models/clustering/parameter_models_atlas.py
+++ src/smartem_models/clustering/parameter_models_atlas.py
@@ -5,16 +5,15 @@ from pydantic import BaseModel
 
 class InitParameters(BaseModel):
     grid_uuid: str
-    batch_size: int = 16
+    batch_size: int = 64
     seed: int = 10
     input_model: Path | None = None
     input_dim: tuple[int, ...] = (1, 64, 64)
     hidden_dims: tuple[int, ...] = (1, 16, 32, 64, 128)
     latent_space_dim: int = 2
     learning_rate: float = 0.0005
-    num_epochs: int = 500
-    model_output_path: str = ""
-    kmeans_output_path: str = ""
+    num_epochs: int = 250
+    model_output_dir: Path | None = None
     num_threads: int = 2
 
 
