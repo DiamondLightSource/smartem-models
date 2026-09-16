7e2a2857299d055f3ef0b49a2ec2f178308db9ba
Daniel Hatton <daniel.hatton@diamond.ac.uk>
2026-09-16 16:36:19 +0100

    parameter name changes

--- src/smartem_models/clustering/parameter_models.py
+++ src/smartem_models/clustering/parameter_models.py
@@ -13,17 +13,15 @@ class InitParameters(BaseModel):
     latent_space_dim: int = 2
     learning_rate: float = 0.0005
     num_epochs: int = 500
-    model_output_path: str = ""
-    kmeans_output_path: str = ""
+    model_output_dir: Path | None = None
     num_threads: int = 2
 
 
 class InferenceParameters(BaseModel):
     grid_uuid: str
-    model_path: Path
+    model_output_dir: Path
     gridsquare_img_path: Path
     gridsquare_uuid: str
-    kmeans_path: str
     input_dim: tuple[int, ...] = (1, 64, 64)
     hidden_dims: tuple[int, ...] = (1, 16, 32, 64, 128)
     latent_space_dim: int = 2
