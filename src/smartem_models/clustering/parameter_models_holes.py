37b8cf867b1fe1fec30447e87a75c020002aff13
Daniel Hatton <daniel.hatton@diamond.ac.uk>
2026-09-16 16:35:40 +0100

    tweaks to default parameters (dae holes)

--- src/smartem_models/clustering/parameter_models_holes.py
+++ src/smartem_models/clustering/parameter_models_holes.py
@@ -13,16 +13,15 @@ class InitParameters(BaseModel):
     latent_space_dim: int = 2
     learning_rate: float = 0.0005
     num_epochs: int = 500
-    model_output_path: str = ""
-    kmeans_output_path: str = ""
+    model_output_dir: Path | None = None
     num_threads: int = 2
     num_squares: int | None = 5
+    init_scores_from_model: str | None = None
 
 
 class InferenceParameters(BaseModel):
     uuid: str  # gridsquare uuid
-    model_path: Path
-    kmeans_path: Path
+    model_output_dir: Path
     input_dim: tuple[int, ...] = (1, 32, 32)
     hidden_dims: tuple[int, ...] = (1, 16, 32, 64, 128)
     latent_space_dim: int = 2
